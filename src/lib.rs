56bebe505801ba0d8f369393ad42c612ec230b8b
Robin Nehls <nehls@mi.fu-berlin.de>
2017-11-01 00:02:42 +0100

    initial comit

---
+++ src/lib.rs
@@ -0,0 +1,199 @@
+extern crate regex;
+
+use std::path::PathBuf;
+use regex::Regex;
+
+//use std::io::{BufRead, BufReader, Read};
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Header {
+    // mandatory data from headers
+    pub artist: String,
+    pub title: String,
+    pub bpm: f32,
+    pub gap: f32,
+    pub audio_path: PathBuf,
+
+    // optional data from headers
+    pub cover_path: Option<PathBuf>,
+    pub background_path: Option<PathBuf>,
+    pub video_path: Option<PathBuf>,
+    pub video_gap: Option<f32>,
+    pub genre: Option<String>,
+    pub edition: Option<String>,
+    pub language: Option<String>,
+    pub year: Option<u32>,
+    pub relative: bool,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Song {
+    pub header: Header,
+    pub lines: Vec<Line>,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum NoteType {
+    Regular,
+    Golden,
+    Freestyle,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Note {
+    pub notetype: NoteType,
+    pub start: i32,
+    pub duration: i32,
+    pub pitch: i32,
+    pub text: String,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Line {
+    pub start: i32,
+    pub notes: Vec<Note>,
+}
+
+fn get_empty_header() -> Header {
+    Header {
+        artist: String::from(""),
+        title: String::from(""),
+        bpm: std::f32::NAN,
+        gap: std::f32::NAN,
+        audio_path: PathBuf::from(""),
+        cover_path: None,
+        background_path: None,
+        video_path: None,
+        video_gap: None,
+        genre: None,
+        edition: None,
+        language: None,
+        year: None,
+        relative: false,
+    }
+}
+
+pub fn parse_txt_header_str(txt_str: &str) -> Result<Header, &str> {
+    let mut found_artist = false;
+    let mut found_title = false;
+    let mut found_bpm = false;
+    let mut found_gap = false;
+    let mut found_audio_path = false;
+
+    let mut header = get_empty_header();
+
+    let re = Regex::new(r"#([A-Z3]*):(.*)").unwrap();
+
+    for line in txt_str.lines() {
+        let cap = match re.captures(line) {
+            Some(x) => x,
+            None => break,
+        };
+        let key = cap.get(1).unwrap().as_str();
+        let value = cap.get(2).unwrap().as_str();
+        match key {
+            "TITLE" => {
+                header.title = String::from(value);
+                if found_title {
+                    return Err("multiple TITLE tags found")
+                }
+                found_title = true;
+            },
+            "ARTIST" => {
+                header.artist = String::from(value);
+                if found_artist {
+                    return Err("multiple ARTIST tags found")
+                }
+                found_artist = true;
+            },
+            "MP3" => {
+                header.audio_path = PathBuf::from(value);
+                if found_audio_path {
+                    return Err("multiple MP3 tags found")
+                }
+                found_audio_path = true;
+            },
+            "GAP" => {
+                header.gap = match value.parse() {
+                    Ok(x) => x,
+                    Err(_) => return Err("invalid GAP"),
+                };
+                if found_gap {
+                    return Err("multiple GAP tags found")
+                }
+                found_gap = true;
+            },
+            "BPM" => {
+                header.bpm = match value.parse() {
+                    Ok(x) => x,
+                    Err(_) => return Err("invalid BPM"),
+                };
+                if found_bpm {
+                    return Err("multiple BPM tags found")
+                }
+                found_bpm = true;
+            },
+
+            // Optional Header fields
+            "COVER" => {
+                header.cover_path = Some(PathBuf::from(value));
+            },
+            "BACKGROUND" => {
+                header.background_path = Some(PathBuf::from(value));
+            },
+            "VIDEO" => {
+                header.video_path = Some(PathBuf::from(value));
+            },
+            "VIDEOGAP" => {
+                header.video_gap = match value.parse() {
+                    Ok(x) => Some(x),
+                    Err(_) => {
+                        println!("Warning: Invalid video gap");
+                        None
+                    },
+                };
+            },
+            "GENRE" => {
+                header.genre = Some(String::from(value));
+                println!("Set genre to: {:?}", header.genre);
+            },
+            "EDITION" => {
+                header.edition = Some(String::from(value));
+                println!("Set edition to: {:?}", header.edition);
+            },
+            "LANGUAGE" => {
+                header.language = Some(String::from(value));
+                println!("Set language to: {:?}", header.language);
+            },
+            "YEAR" => {
+                header.year = match value.parse() {
+                    Ok(x) => Some(x),
+                    Err(_) => {
+                        println!("Warning: Invalid year");
+                        None
+                    },
+                };
+                println!("Set video_gap to: {:?}", header.video_gap);
+            },
+            "RELATIVE" => {
+                header.relative = match value {
+                    "YES" => true,
+                    "NO" => false,
+                    _ => { println!("Warning: Invalid relative tag");
+                    false
+                },
+            };
+            println!("Set video_gap to: {:?}", header.video_gap);
+        },
+
+        _ => println!("{}",key),
+    };
+
+}
+Ok(header)
+}
+
+pub fn parse_txt_lines_str(txt_str: &str) -> Result<Vec<Line>, &str> {
+    Ok(Vec::new())
+
+}
---
+++ tests/txt.rs
@@ -0,0 +1,132 @@
+extern crate ultrastar_txt;
+
+use ultrastar_txt::*;
+use std::path::PathBuf;
+
+#[test]
+fn parse_simple_txt_header_str() {
+    let txt = get_simple_txt_str();
+    let header = get_simple_txt_header();
+    assert_eq!(header, parse_txt_header_str(txt).unwrap());
+}
+
+#[test]
+fn parse_simple_txt_lines_str() {
+    let txt = get_simple_txt_str();
+    let lines = get_simple_txt_lines();
+    assert_eq!(lines, parse_txt_lines_str(txt).unwrap());
+}
+
+#[test]
+fn komma_in_float_number() {
+    let txt = include_str!("komma_in_float.txt");
+    assert!(parse_txt_header_str(txt).is_ok())
+}
+
+fn get_simple_txt_str() -> &'static str {
+    include_str!("simple_txt_with_all_features.txt")
+}
+
+fn get_simple_txt_header() -> Header {
+    Header {
+        artist: String::from("Testartist"),
+        title: String::from("Testsong"),
+        bpm: 123.0,
+        gap: 666.0,
+        audio_path: PathBuf::from("Testfile.mp3"),
+        relative: false,
+        video_path: Some(PathBuf::from("DLzxrzFCyOs.mp4")),
+        cover_path: Some(PathBuf::from("Cover.jpg")),
+        background_path: Some(PathBuf::from("BG.jpg")),
+        video_gap: Some(777.0),
+        genre: Some(String::from("Music")),
+        edition: Some(String::from("Testmusic")),
+        language: Some(String::from("en")),
+        year: Some(1337),
+    }
+}
+
+fn get_simple_txt_lines() -> Vec<Line> {
+    vec![
+    Line {
+        start: 0,
+        notes: vec![
+        Note {
+            notetype: NoteType::Regular,
+            start: 0,
+            duration: 4,
+            pitch: 59,
+            text: String::from("Test "),
+        },
+        Note {
+            notetype: NoteType::Regular,
+            start: 4,
+            duration: 4,
+            pitch: 59,
+            text: String::from("I"),
+        },
+        Note {
+            notetype: NoteType::Regular,
+            start: 8,
+            duration: 4,
+            pitch: 59,
+            text: String::from("'m"),
+        },
+        Note {
+            notetype: NoteType::Golden,
+            start: 12,
+            duration: 4,
+            pitch: 59,
+            text: String::from("test"),
+        },
+        Note {
+            notetype: NoteType::Regular,
+            start: 16,
+            duration: 4,
+            pitch: 59,
+            text: String::from("ing."),
+        },
+        ],
+    },
+    Line {
+        start: 20,
+        notes: vec![
+        Note {
+            notetype: NoteType::Regular,
+            start: 24,
+            duration: 4,
+            pitch: 59,
+            text: String::from("Test "),
+        },
+        Note {
+            notetype: NoteType::Regular,
+            start: 28,
+            duration: 4,
+            pitch: 59,
+            text: String::from("I"),
+        },
+        Note {
+            notetype: NoteType::Regular,
+            start: 32,
+            duration: 4,
+            pitch: 59,
+            text: String::from("'m"),
+        },
+        Note {
+            notetype: NoteType::Freestyle,
+            start: 36,
+            duration: 4,
+            pitch: 59,
+            text: String::from("test"),
+        },
+        Note {
+            notetype: NoteType::Freestyle,
+            start: 40,
+            duration: 4,
+            pitch: 59,
+            text: String::from("ing."),
+        },
+        ],
+    },
+    ]
+}
