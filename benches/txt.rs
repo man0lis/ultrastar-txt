19feb44390b81e35a7c726fb9f52bc135402f622
Robin Nehls <nehls@mi.fu-berlin.de>
2017-11-16 01:07:24 +0100

    add benchmark for generator

--- benches/txt.rs
+++ benches/txt.rs
@@ -26,6 +26,28 @@ fn bench_parse_real_song_str(b: &mut Bencher) {
     });
 }
 
+#[bench]
+fn bench_generate_simple_song_str(b: &mut Bencher) {
+    let txt = get_simple_txt_str();
+    let header = parse_txt_header_str(txt).unwrap();
+    let lines = parse_txt_lines_str(txt).unwrap();
+    b.iter(|| {
+        let song_txt = generate_song_txt(&header, &lines).unwrap();
+        black_box(song_txt);
+    });
+}
+
+#[bench]
+fn bench_generate_real_song_str(b: &mut Bencher) {
+    let txt = get_real_txt_str();
+    let header = parse_txt_header_str(txt).unwrap();
+    let lines = parse_txt_lines_str(txt).unwrap();
+    b.iter(|| {
+        let song_txt = generate_song_txt(&header, &lines).unwrap();
+        black_box(song_txt);
+    });
+}
+
 fn get_simple_txt_str() -> &'static str {
     include_str!("simple_txt_with_all_features.txt")
 }
